06704ef53e81ddbcb6aae4b7d14dd5834202d847
showthesunli <showthesunli@gmail.com>
2025-03-26 00:49:59 +0800

    style: Add encoding to FileHandler and format code in logging.py

--- telegram_logger/utils/logging.py
+++ telegram_logger/utils/logging.py
@@ -2,6 +2,7 @@ import logging
 import sys
 from telegram_logger.config import DEBUG_MODE
 
+
 def configure_logging():
     """配置全局日志记录"""
     level = logging.DEBUG if DEBUG_MODE else logging.INFO
@@ -10,8 +11,8 @@ def configure_logging():
         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
         handlers=[
-            logging.FileHandler("tg_logger.log"),
-            logging.StreamHandler(sys.stdout)
-        ]
+            logging.FileHandler("tg_logger.log", encoding="utf-8"),
+            logging.StreamHandler(sys.stdout),
+        ],
     )
     logging.getLogger("telethon").setLevel(logging.WARNING)
